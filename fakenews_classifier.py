4120fb9e0ca944ae177df5ad8c34b9c9a5b79174
Ruhma Mehek Khan <42090311+ruhmamehek@users.noreply.github.com>
2020-03-31 17:37:51 +0530

    Update fakenews_classifier.py

--- fakenews_classifier.py
+++ fakenews_classifier.py
@@ -8,61 +8,103 @@ from nltk.tokenize import RegexpTokenizer
 from nltk.stem import WordNetLemmatizer
 from nltk.stem.porter import PorterStemmer
 
-nltk.download('stopwords')
-nltk.download('wordnet')
-
-
-
 df = pd.read_csv("politifact_fake.csv")
+# reading data from csv file. df is a dataframe consisting 
+# of fake news that we will use to train our model.
 dr = pd.read_csv("politifact_real.csv")
+# reading data from csv file. dr is a dataframe consisting 
+# of real news that we will use to train our model.
+
 
 
 tokenizer=RegexpTokenizer(r'\w+')
 dr['title']=dr['title'].apply(lambda x: tokenizer.tokenize(x.lower()))
 df['title']= df['title'].apply(lambda x: tokenizer.tokenize(x.lower()))
+# tokenizing the data, i.e. splitting the title into substrings using a regular expression.
 
 
 def remove_stopWords(text):
+  """
+  Removes stop words from the list of words provide
+  
+  Args:
+    text: list of words (strings)
+    
+    Returns: list of words (strings), after removing stopwords
+    
+  """
   words = [w for w in text if w not in stopwords.words('english')]
   return words
 
 lemmatizer = WordNetLemmatizer()
+
 def word_lemmatizer(text):
+    """
+     groups different inflected forms of a word together
+    
+    Args:
+      text: list of words (strings)
+      
+    Returns: list of words (strings), after performing lemmatization
+    
+    """
   lem_text = [lemmatizer.lemmatize(i) for i in text]
   return (lem_text)
 
 stemmer = PorterStemmer()
+
 def word_stemmer(text):
+  
+    """
+     converts words to its root/base form
+    
+    Args:
+      text: list of words (strings)
+      
+    Returns: list of words, after stemming each string
+    
+    """
+  
   stem_text = "".join([stemmer.stem(i) for i in text ])
   return stem_text
+
 def remove_punctuation(text):
+  
+     """
+     removes punctuation marks from text
+    
+    Args:
+      text: list of words (strings)
+      
+    Returns: list of words (strings), after removing strings that were punctuation marks
+    """
+    
+  
   no_punct = "".join([c for c in text if c not in string.punctuation])
   return no_punct
 
-# print("preprocessed real titles ")
-# print(dr['title'])
-# print("preprocessed fake titles ")
-# print(df['title'])
 
-# dr['title']=dr['title'].apply(lambda x: remove_punctuation(x))
 dr['title']= dr['title'].apply(lambda x: remove_stopWords(x))
 dr['title']=dr['title'].apply(lambda x: word_lemmatizer(x))
-# dr['title']=dr['title'].apply(lambda x: word_stemmer(x))
 
-# df['title']=dr['title'].apply(lambda x: remove_punctuation(x))
+
 df['title']=df['title'].apply(lambda x: remove_stopWords(x))
 df['title']=df['title'].apply(lambda x: word_lemmatizer(x))
-# df['title']=df['title'].apply(lambda x: word_stemmer(x))
+
 
 arr_real=[]
 for i in dr['title']:
   arr_real.append((list(i), 'real'))
+  
+# adding tuples of the processed real news titles, with the string 'real' to a list
 
-# print('arr_real', arr_real)
+
 arr_fake=[]
 for i in df['title']:
   arr_fake.append((list(i), 'fake'))
 # print('arr_fake', arr_fake)
+
+# adding tuples of the processed fake news titles, with the fake 'real' to a list
 
 
 all_words=[]
@@ -76,12 +118,16 @@ all_words = [item for sublist in all_words for item in sublist]
 all_titles=[]
 all_titles=arr_fake+arr_real
 random.shuffle(all_titles)
-
-# print("all titles", all_titles)
-# print(all_words)
+# making a list of all titles in dataset, and shuffling them
 
 
 def document_features(document):
+    """
+    creates a feature set, checking the words in the provided documents 
+    and marking their presence as True or False. 
+    This function is used in this code to create feature sets.
+    
+    """"
     document_words = set(document)
     features = {}
     for word in all_words:
@@ -90,7 +136,11 @@ def document_features(document):
 
 featuresets = [(document_features(d), c) for (d,c) in all_titles]
 train_set, test_set = featuresets[:800], featuresets[800:]
+# dividing dataset into training and dataset
+
 classifier = nltk.NaiveBayesClassifier.train(train_set)
+# making a Naive Bayes classifier using our training data
+
 print(nltk.classify.accuracy(classifier, test_set))
 
 headlineTest = input("Enter Headline to classify: ")
